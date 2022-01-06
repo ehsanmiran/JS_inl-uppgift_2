cc4254aa1164a3fdc332c1af978e3cb5dc3c4aaf
ehsanmiran <92309474+ehsanmiran@users.noreply.github.com>
2022-01-06 02:44:12 +0100

    input/validate/doc.output/doc.remove all working now

--- inlämningsuppgift1/script.js
+++ inlämningsuppgift1/script.js
@@ -1,17 +1,30 @@
 let usersListArray = [];
+
+const outputDiv = document.querySelector('#listToAdd');
 const regForm = document.querySelector('#regForm');
 const email = document.querySelector('#email');
-const outputDiv = document.querySelector('#listToAdd');
-
-var boolean1 = 'false';
-var boolean2 = 'false';
-var boolean3 = 'false';
 
 
+const usersList = () => {                       // ------ intiate (add new <div>) to the doc ------
+  outputDiv.innerHTML = '';
+  usersListArray.forEach(user => {
+    outputDiv.innerHTML += `
+    <div id="${user.id}" class="d-flex justify-content-between align-items-center bg-white p-2 mb-2">
+      <div>  
+        <p class="m-0 h5">${user.firstName} ${user.lastName}</p>
+        <p class="m-0">${user.email}</p>
+      </div>
+      <div>
+        <button type="button" class="btn btn-info btn-sm px-3">EDIT</button>
+        <button type="button" class="btn btn-danger btn-sm">DELETE</button>
+    </div>
+    `;
+  })
+}
 
 const validateText = (id) => {                  // ------ Text Validations -------
   let input = document.querySelector(id);
-
+  
   if(input.value === '' || input.value.length < 2) {
     input.classList.remove('is-valid');
     input.classList.add('is-invalid');
@@ -47,18 +60,15 @@ const validateEmail = (emailInput) => {         // ------ Email Validations ----
   }
 }
 
-
 email.addEventListener('keyup', () => {         // ------ Guide text lines under fields ------
-    validateEmail(email)
-  })
+  validateEmail(email)
+})
 firstName.addEventListener('keyup', () => {
-    validateText('#firstName')
-  })
+  validateText('#firstName')
+})
 lastName.addEventListener('keyup', () => {
-    validateText('#lastName')
-  })
-
-
+  validateText('#lastName')
+})
 
 regForm.addEventListener('submit', e => {       // ------ SUBMISSION ------
   e.preventDefault();
@@ -68,49 +78,29 @@ regForm.addEventListener('submit', e => {       // ------ SUBMISSION ------
   validateEmail(email);
   
   if(boolean1 === 'true' && boolean2 === 'true' && boolean3 === 'true') {
-    let user = {
-      id: (new Date()).getTime(),
-      firstName: e.currentTarget.firstName.value,
-      lastName: e.currentTarget.lastName.value,
-      email: e.currentTarget.email.value,
+    const user = {
+      id: Date.now().toString(),
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
     }
     console.log(user);
     usersListArray.push(user);                  // ------ Adding element to Array for each User ------
     console.log(usersListArray);
-  
-
-    const usersList = () => {
-      let i = 0;
-      outputDiv.innerHTML = '';
-      usersListArray.forEach(user => {
-        const userInUse = usersListArray[i];
-        outputDiv.innerHTML += `
-        <div id="${userInUse.id}" class="d-flex justify-content-between align-items-center bg-white p-2 mb-2">
-        <div>  
-        <p class="m-0 h5">${userInUse.firstName} ${userInUse.lastName}</p>
-        <p class="m-0">${userInUse.email}</p>
-        </div>
-        <div>
-        <button type="button" class="btn btn-info btn-sm px-3">EDIT</button>
-        <button type="button" class="btn btn-danger btn-sm">DELETE</button>
-        </div>
-        `;
-        i++;
-      })
-    }
+    
     usersList();
     document.getElementById("regForm").reset();
     boolean1 = 'false';
     boolean2 = 'false';
-
-    outputDiv.addEventListener('click', e => {
-      if(e.target.type === 'button') {
-        console.log('it is a button');
-        //--- below is the - Array.filter - method.
-        usersListArray = usersListArray.filter(user => userInUse.id !== e.target.parentNode.id);
-        usersList();
-      }
-    })
+    boolean3 = 'false';
   }
 })
 
+outputDiv.addEventListener('click', e => {
+  console.log(e.target.parentNode.parentNode.id);
+  if(e.target.type === 'button') {
+
+    usersListArray = usersListArray.filter(user => user.id !== e.target.parentNode.parentNode.id); //--- the (Array.filter) method.
+    usersList();
+  }
+})
